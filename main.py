d4ce292036582716c3b7b98aafb5fc42b2ebcddc
Kre0ns <yonpun13@gmail.com>
2025-01-12 23:04:24 +0200

    Add basic streamlit web interface

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.header("Welcome to the Reddit sentiment analyser!")
+
+post = st.text_input("Enter Reddit post link:")
+
+if st.button("Analyse"):
+    if post.strip():  
+
+        st.write("Lorem ipsum")
+
+    else:
+        st.error("Please enter a valid Reddit post link before analysing.")
