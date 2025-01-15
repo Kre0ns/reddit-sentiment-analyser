402c5bc092202912fb0fbbbd0bfd60ba6f5e3740
Kre0ns <yonpun13@gmail.com>
2025-01-15 10:36:39 +0200

    Reformat with black

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ st.header("Welcome to the Reddit sentiment analyser!")
 post = st.text_input("Enter Reddit post link:")
 
 if st.button("Analyse"):
-    if post.strip():  
+    if post.strip():
 
         st.write("Lorem ipsum")
 
     else:
+
         st.error("Please enter a valid Reddit post link before analysing.")
