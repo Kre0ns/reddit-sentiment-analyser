1ee378bb0de31ff25c1fcd39917711537dba43f4
Kre0ns <yonpun13@gmail.com>
2025-01-16 00:34:46 +0200

    Update UI in main.py

--- main.py
+++ main.py
@@ -2,13 +2,26 @@ import streamlit as st
 
 st.header("Welcome to the Reddit sentiment analyser!")
 
-post = st.text_input("Enter Reddit post link:")
+col1, col2 = st.columns([1, 1])
+
+with col1:
+    subreddit_name = st.text_input(
+        'Subreddit name with no "r/" (Bad: "r/test", Good: "test"):'
+    )
+
+with col2:
+    amount_to_scrape = st.number_input(
+        "Amount to analyse:",
+        min_value=1,
+        max_value=50,
+        value=1,
+        step=1,
+    )
 
 if st.button("Analyse"):
-    if post.strip():
+    if subreddit_name.strip():
 
         st.write("Lorem ipsum")
 
     else:
-
-        st.error("Please enter a valid Reddit post link before analysing.")
+        st.error("Please enter an existing subreddit name!a")
